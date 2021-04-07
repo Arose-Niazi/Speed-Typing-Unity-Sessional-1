78f8276b02d8b1d57bca3ae78e5c2ad7cd3dc83a
Arose Niazi <arose.niazi@yahoo.com>
2021-04-07 19:26:54 +0500

    Initial commit

---
+++ Assets/Scripts/Alphabets.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Alphabets : MonoBehaviour
+{
+    private int _bestTimeMin;
+    private int _bestTimeSec;
+
+    public Text bestTime;
+    public Text text;
+    public Text timeField;
+    private int _timeMin;
+    private int _timeSec;
+    private int _pos;
+
+    private string[] keysOrder =
+    {
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+        "w", "x", "y", "z"
+    };
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _bestTimeMin = PlayerPrefs.GetInt("Min", 0);
+        _bestTimeSec = PlayerPrefs.GetInt("Sec", 0);
+
+        bestTime.text = String.Format("Best Time: "+_bestTimeMin+":"+_bestTimeSec);
+        _timeMin = 0;
+        _timeSec = 0;
+        _pos = 0;
+        
+        InvokeRepeating("Timer", 1f, 1f);
+    }
+
+    public void Timer()
+    {
+        if (_pos != keysOrder.Length)
+            _timeSec++;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (_pos == keysOrder.Length)
+            return;
+        if (Input.GetKeyDown(keysOrder[_pos]))
+        {
+            text.text = text.text.Substring(1);
+            _pos++;
+        }
+        else
+        {
+            for(int i=0; i<keysOrder.Length; i++)
+            {
+                if (i == _pos)
+                    continue;
+                if(Input.GetKeyDown(keysOrder[i]))
+                {
+                    _timeSec += 5;
+                }
+            }
+        }
+
+        if (_timeSec == 60)
+        {
+            _timeSec = 0;
+            _timeMin++;
+        }
+
+        timeField.text = String.Format("Time: "+_timeMin+":"+_timeSec);
+
+        if (_pos == keysOrder.Length)
+        {
+            if (_timeMin < _bestTimeMin)
+            {
+                PlayerPrefs.SetInt("Min", _timeMin);
+                PlayerPrefs.SetInt("Sec", _timeSec);
+            }
+            else if (_timeMin == _bestTimeMin)
+            {
+                if (_timeSec < _bestTimeSec)
+                {
+                    PlayerPrefs.SetInt("Min", _timeMin);
+                    PlayerPrefs.SetInt("Sec", _timeSec);
+                }
+            }
+            else if (_bestTimeSec == 0 && _bestTimeMin == 0)
+            {
+                PlayerPrefs.SetInt("Min", _timeMin);
+                PlayerPrefs.SetInt("Sec", _timeSec);
+            }
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+
+    public static float GameVolume;
+    public static string PlayerName;
+
+    public Slider volumeSetting;
+    public AudioSource soundSource;
+
+    public InputField playerNameField;
+
+    public Text playerNameText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (volumeSetting != null)
+        {
+            GameVolume = volumeSetting.value;
+        }
+        
+        if (playerNameField != null)
+        {
+            PlayerName = playerNameField.text;
+        }
+
+        if (playerNameText != null)
+        {
+            playerNameText.text = PlayerName;
+        }
+        soundSource.volume = GameVolume;
+
+        if (Input.GetKeyDown("escape"))
+        {
+            PauseGame();
+        }
+        
+    }
+    
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("GamePaly");
+    }
+
+    private void PauseGame()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
